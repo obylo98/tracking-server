6eb0a2ee2efca0b75fd2ccc612bc5f7a4afceb1b
Obinna Andrew Onyekwere <obinnaandrewonyekwere@gmail.com>
2023-06-28 15:59:06 +0300

    changes

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ def tracking_pixel():
     open_time = datetime.datetime.now()
     location = request.headers.get('X-Forwarded-For', request.remote_addr)
 
+    return send_file('pixel.png', mimetype='image/png', add_etags=False)
+
+@app.route('/pixel.png')
+def pixel_image():
+    device = request.user_agent.string
+    open_time = datetime.datetime.now()
+    location = request.headers.get('X-Forwarded-For', request.remote_addr)
+
     log_entry = {
         'device': device,
         'open_time': open_time,
@@ -19,10 +27,15 @@ def tracking_pixel():
 
     log_data.append(log_entry)
 
-    return send_file('pixel.png', mimetype='image/png')
+    return send_file('pixel.png', mimetype='image/png', add_etags=False)
 
 @app.route('/log')
 def view_log():
+    now = datetime.datetime.now()
+    for log_entry in log_data:
+        duration = now - log_entry['open_time']
+        log_entry['duration'] = duration.total_seconds()
+
     return render_template('log.html', log_entries=log_data)
 
 if __name__ == '__main__':
